dc592ed8c4cd3b8993be5b94c5a3ebecde9b6252
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-05-01 17:51:55 -0600

    Agregar archivos de proyecto.

---
+++ XML_console/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Xml;
+
+
+namespace XML_console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string URLString = @"C:\xml\Books.xml";
+
+			XmlTextReader reader = new XmlTextReader(URLString);
+
+			while (reader.Read())
+			{
+				switch (reader.NodeType)
+				{
+					case XmlNodeType.Element: // The node is an element.
+						Console.Write("<" + reader.Name);
+
+						while (reader.MoveToNextAttribute()) // Read the attributes.
+							Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+						Console.Write(">");
+
+						break;
+					case XmlNodeType.Text: //Display the text in each element.
+						Console.WriteLine(reader.Value);
+						break;
+					case XmlNodeType.EndElement: //Display the end of the element.
+						Console.Write("</" + reader.Name);
+						Console.WriteLine(">");
+						break;
+				}
+			}
+		}
+
+	}
+}
+
